17c85d2f803c1a5492e618e54663d4b2ccc85dee
alejandrodiazpinilla <diazjohn83@gamil.com>
2019-06-13 20:25:33 -0500

    se puso un parametro

--- solution.rb
+++ solution.rb
@@ -1,9 +1,27 @@
+# require 'sinatra'
+
+# get '/' do
+#     if params[:nombre] == "Juan"
+#         "<h1>Hola #{params[:nombre]}!</h1>" 
+#     else
+#        "<h1> Hola desconocido! </h1>" 
+#     end     
+# end
+
+
+# require 'sinatra' Solucion maker
+
+# get '/' do
+#   if params[:nombre].present?
+#     "<h1>Hola #{params[:nombre].capitalize}!</h1>"
+#   else
+#     "<h1>Hola desconocido!</h1>"
+#   end
+# end
+
+
 require 'sinatra'
 
-get '/' do
-    if params[:nombre] == "Juan"
-        "<h1>Hola #{params[:nombre]}!</h1>" 
-    else
-       "<h1> Hola desconocido! </h1>" 
-    end     
-end
+get '/makers/:nombre' do
+      "<h1>Hola #{params[:nombre].capitalize}!</h1>"
+  end
